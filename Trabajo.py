9319a4332734ff8b2f3a8c23d5d5c1c5755b7b82
LucasMDoval <martindovallucas@gmail.com>
2023-10-27 13:14:48 +0200

    Update Trabajo.py

--- Trabajo.py
+++ Trabajo.py
@@ -1,4 +1,17 @@
 import random
 
 palabra = ["Tembloroso", "Engranaje", "Independencia", "Colarse", "Corto", "Gritar", "Multimillonario", "Sonic", "Disculparse", "Prueba"]
-palabra_generada = 
+palabra_generada = random.choice (palabra)
+
+letra = input(f"La palabra genrada tiene {len(palabra_generada)} letras, elige una letra")
+
+intentos = 5 
+
+if letra in palabra_generada:
+    letra_correcta = input("Esa letra está en la palabra, elige otra otra")
+else:
+    letra_incorrecta = input(f"Esa letra no está en la palabra, te quedan {intentos-1}")
+
+if intentos == 0:
+    print(f"No te quedan intentos! La palabra elegida era {palabra_generada}")
+    
