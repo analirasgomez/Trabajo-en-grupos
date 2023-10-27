3014b53fb469d2c439267640faac53e4649b50ed
PabloAlda <pabloam0309@gmail.com>
2023-10-27 13:13:58 +0200

    trabajo

--- Trabajo.py
+++ Trabajo.py
@@ -1,3 +1,7 @@
+#El programa que vamos a crees va a consistir en un juego para adivinar la palabra correcta.
+print("")
+
+
 import random
 
 palabra = ["Tembloroso", "Engranaje", "Independencia", "Colarse", "Corto", "Gritar", "Multimillonario", "Sonic", "Disculparse", "Prueba"]
