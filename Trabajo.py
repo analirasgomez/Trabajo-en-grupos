1310dd59a3dbd90a4cff39acec17eb0b732ff3b6
LucasMDoval <martindovallucas@gmail.com>
2023-11-21 19:24:15 +0100

    Update Trabajo.py

--- Trabajo.py
+++ Trabajo.py
@@ -15,36 +15,71 @@
 
 
 #El programa que vamos a crees va a consistir en un juego para adivinar la palabra correcta.
-print("")
+print("Bienvenido! En este juego deberás adivinar una palagra generada al azar, tienes 5 intentos en los cuales puedes ir diciendo letras que creas que pueden estar en la palabra, si no aciertas la palabra en los 5 intentos habras perdido. Suerte!")
 
 
 import random
 
-palabra = ["Tembloroso", "Engranaje", "Independencia", "Colarse", "Corto", "Gritar", "Multimillonario", "Sonic", "Disculparse", "Prueba"]
-palabra_generada = random.choice (palabra)
+class JuegoAdivinanza:
+    def __init__(self):
+        # Lista de palabras para el juego
+        self.palabras = ["Tembloroso", "Engranaje", "Independencia", "Colarse", "Corto", "Gritar", "Multimillonario", "Sonic", "Disculparse", "Prueba"]
+        # Número de intentos disponibles
+        self.intentos = 5
+        # Palabra seleccionada al azar para el juego
+        self.palabra_generada = random.choice(self.palabras)
+        # Conjunto para almacenar las letras adivinadas, evitando duplicados
+        self.letras_adivinadas = set()
 
-letra = input(f"La palabra genrada tiene {len(palabra_generada)} letras, elige una letra o intenta adivinar la palabra: ")
+    def mostrar_palabra_oculta(self):
+        # Construir la representación de la palabra oculta con letras adivinadas y guiones bajos
+        palabra_oculta = ""
+        for letra in self.palabra_generada:
+            if letra in self.letras_adivinadas:
+                palabra_oculta += letra
+            else:
+                palabra_oculta += "_"
+        return palabra_oculta
 
-intentos = 5 
+    def jugar(self):
+        # Bucle principal del juego
+        while self.intentos > 0:
+            # Mostrar la palabra oculta y los intentos restantes, y obtener la entrada del jugador
+            letra = input(f"Palabra: {self.mostrar_palabra_oculta()} - Intentos restantes: {self.intentos}. Elige una letra o intenta adivinar la palabra: ")
 
-if letra in palabra_generada:
-    letra_correcta = input("Esa letra está en la palabra, elige otra otra")
-else:
-    letra_incorrecta = input(f"Esa letra no está en la palabra, te quedan {intentos-1}")
+            if letra in self.letras_adivinadas:
+                # La letra ya ha sido elegida
+                print("Ya has elegido esa letra. Intenta con otra.")
+                continue
 
+            if len(letra) == 1 and letra.isalpha():
+                # El jugador eligió una letra
+                self.letras_adivinadas.add(letra)
+                if letra in self.palabra_generada:
+                    print("¡Letra correcta!")
+                else:
+                    print(f"Letra incorrecta. Te quedan {self.intentos - 1} intentos.")
+                    self.intentos -= 1
+            elif len(letra) == len(self.palabra_generada) and letra.isalpha():
+                # El jugador intenta adivinar la palabra completa
+                if letra == self.palabra_generada:
+                    print(f"¡Enhorabuena! Has adivinado la palabra: {self.palabra_generada}")
+                    break
+                else:
+                    print("Palabra incorrecta. ¡Inténtalo de nuevo!")
+                    self.intentos -= 1
+            else:
+                # Entrada no válida
+                print("Entrada no válida. Por favor, elige una letra o intenta adivinar la palabra.")
 
-if letra_correcta or letra_incorrecta in palabra_generada:
-    letra_correcta = input("Esa letra está en la palabra, elige otra otra")
-else:
-    letra_incorrecta = input(f"Esa letra no está en la palabra, te quedan {intentos-1}")
+        # Verificar si el jugador se quedó sin intentos
+        if self.intentos == 0:
+            print(f"No te quedan intentos. La palabra correcta era: {self.palabra_generada}")
 
+# Crear un objeto del juego
+juego = JuegoAdivinanza()
 
-if letra or letra_correcta or letra_incorrecta == palabra_generada:
-    print(f"Enhorabuena! has acertado la palabra {palabra_generada}")
-
-if intentos == 0:
-    print(f"No te quedan intentos! La palabra elegida era {palabra_generada}")
-
-
+# Iniciar el juego
+juego.jugar()
 
 #hacer menú de si quieres volver a jugar
